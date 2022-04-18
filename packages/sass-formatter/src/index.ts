388bd16b4e97e3f968851a67344a4b3ba9dd9539
johnsoncodehk <johnsoncodehk@gmail.com>
2022-04-18 08:51:04 +0800

    chore: format

--- packages/sass-formatter/src/index.ts
+++ packages/sass-formatter/src/index.ts
@@ -12,12 +12,12 @@ export = function (configs: Parameters<typeof SassFormatter.Format>[1]): Embedde
 
 			const _options: typeof configs = {
 				...configs,
-                insertSpaces: options.insertSpaces,
+				insertSpaces: options.insertSpaces,
 			};
 
 			// don't set when options.insertSpaces is false to avoid sass-formatter internal judge bug
-            if (options.insertSpaces)
-                _options.tabSize = options.tabSize;
+			if (options.insertSpaces)
+				_options.tabSize = options.tabSize;
 
 			const oldText = document.getText(range);
 			const newText = SassFormatter.Format(oldText, _options);
