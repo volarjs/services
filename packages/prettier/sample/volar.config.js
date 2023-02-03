7b2cce13066bec9505340f4bc8983a124c6ee445
Johnson Chu <johnsoncodehk@gmail.com>
2023-02-03 19:41:39 +0800

    refactor(prettier): rebrand plugin interface (#23)

---
+++ packages/prettier/sample/volar.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins: [
+		require('..')({
+			html: {
+				// breakContentsFromTags: true,
+			},
+			// ignoreIDEOptions: true,
+		}),
+	],
+};
--- packages/prettier/src/index.ts
+++ packages/prettier/src/index.ts
@@ -1,90 +1,76 @@
 import type { LanguageServicePlugin } from '@volar/language-service';
 import { format, resolveConfigFile, resolveConfig, Options } from 'prettier';
-import { URI } from 'vscode-uri';
 
-export interface VolarPrettierConfig {
-	/**
-	 * Languages to be formatted by prettier.
-	 *
-	 * @default
-	 * ['html', 'css', 'scss', 'typescript', 'javascript']
-	 */
-	languages?: string[];
-	html?: {
+const plugin = (
+	options: {
 		/**
-		 * Preprocessing to break "contents" from "HTML tags".
-		 * This will prevent HTML closing tags, and opening tags without attributes
-		 * from breaking into a blank `>` or `<` on a new line.
+		 * Languages to be formatted by prettier.
+		 *
+		 * @default
+		 * ['html', 'css', 'scss', 'typescript', 'javascript']
 		 */
-		breakContentsFromTags?: boolean;
-	};
-	/**
-	 * Use settings from VSCode's `editor.tabSize` and temporary tabSize on status bar
-	 *
-	 * @see https://github.com/volarjs/plugins/issues/5
-	 */
-	useVscodeIndentation?: boolean;
-}
+		languages?: string[];
+		html?: {
+			/**
+			 * Preprocessing to break "contents" from "HTML tags".
+			 * This will prevent HTML closing tags, and opening tags without attributes
+			 * from breaking into a blank `>` or `<` on a new line.
+			 */
+			breakContentsFromTags?: boolean;
+		};
+		/**
+		 * Do not use settings from VSCode's `editor.tabSize` and temporary tabSize on status bar
+		 *
+		 * @see https://github.com/volarjs/plugins/issues/5
+		 */
+		ignoreIDEOptions?: boolean,
+	},
+	getPrettierConfig = () => {
+		const prettierConfigFile = resolveConfigFile.sync();
+		const prettierConfig = (prettierConfigFile ? resolveConfig.sync(prettierConfigFile) : null) || {};
+		return prettierConfig;
+	},
+): LanguageServicePlugin => (context) => {
 
-export const defaultConfig: VolarPrettierConfig = {
-	languages: ['html', 'css', 'scss', 'typescript', 'javascript'],
-};
+	options.languages ??= ['html', 'css', 'scss', 'typescript', 'javascript'];
 
-function mapDefault<T>(o: T, d: T): T {
-	for (const k in d) {
-		if (typeof o[k] === 'undefined') {
-			o[k] = d[k];
-		}
-		if (
-			typeof d[k] === 'object' &&
-			Object.getPrototypeOf(d[k]) === Object.prototype
-		) {
-			mapDefault(o[k], d[k]);
-		}
-	}
+	const prettierOptions = getPrettierConfig();
 
-	return o;
-}
+	return {
+		format(document, _, formatOptions) {
 
-export const volarPrettierPlugin: (
-	config: VolarPrettierConfig
-) => LanguageServicePlugin = (config = {}) => {
-	mapDefault(config, defaultConfig);
-
-	const prettierConfigFile = resolveConfigFile.sync();
-	const prettierConfig =
-		(prettierConfigFile ? resolveConfig.sync(prettierConfigFile) : null) || {};
-
-	return () => ({
-		format(document, _, opts) {
-			if (!config.languages || !config.languages.includes(document.languageId))
+			if (!options.languages || !options.languages.includes(document.languageId)) {
 				return;
+			}
 
 			const fullText = document.getText();
 			let oldText = fullText;
 
 			const isHTML = document.languageId === 'html';
-			if (isHTML && config.html?.breakContentsFromTags) {
+			if (isHTML && options.html?.breakContentsFromTags) {
 				oldText = oldText
 					.replace(/(<[a-z][^>]*>)([^ \n])/gi, '$1 $2')
 					.replace(/([^ \n])(<\/[a-z][a-z0-9\t\n\r -]*>)/gi, '$1 $2');
 			}
 
 			const currentPrettierConfig: Options = {
-				...prettierConfig,
-				filepath: URI.parse(document.uri).fsPath + (isHTML ? '.vue' : ''),
+				...prettierOptions,
+				filepath: context.uriToFileName(document.uri),
 			};
 
-			if (config.useVscodeIndentation) {
-				currentPrettierConfig.useTabs = !opts.insertSpaces;
-				currentPrettierConfig.tabWidth = opts.tabSize;
+			if (!options.ignoreIDEOptions) {
+				currentPrettierConfig.useTabs = !formatOptions.insertSpaces;
+				currentPrettierConfig.tabWidth = formatOptions.tabSize;
 			}
 
 			let newText = format(oldText, currentPrettierConfig);
 
 			newText = '\n' + newText.trim() + '\n';
 
-			if (newText === oldText) return [];
+			if (newText === oldText) {
+				return [];
+			}
+
 			return [
 				{
 					range: {
@@ -95,7 +81,7 @@ export const volarPrettierPlugin: (
 				},
 			];
 		},
-	});
+	};
 };
 
-export default volarPrettierPlugin;
+export = plugin;
