dd22aaf2c221fbe8a538f3866285be3e33a8fc1d
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-05 18:16:40 +0800

    fix: emmet, pug web module export symbol incorrect

--- packages/emmet/src/empty.ts
+++ packages/emmet/src/empty.ts
@@ -2,4 +2,4 @@ import type { LanguageServicePlugin } from '@volar/language-service';
 
 console.warn('@volar-plugins/emmet: This plugin is not support on web yet.')
 
-export = (): LanguageServicePlugin => () => ({});
+export default (): LanguageServicePlugin => () => ({});
--- packages/pug/src/empty.ts
+++ packages/pug/src/empty.ts
@@ -2,4 +2,4 @@ import type { LanguageServicePlugin } from '@volar/language-service';
 
 console.warn('@volar-plugins/pug: This plugin is not support on web yet.')
 
-export = (): LanguageServicePlugin => () => ({});
+export default (): LanguageServicePlugin => () => ({});
