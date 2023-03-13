255950fd8f8a7794eed0143629b66f5828c6e8d9
Vitaly Turovsky <vital2580@icloud.com>
2023-03-13 22:17:19 +0300

    tabs

--- packages/typescript/src/configs/getFormatCodeSettings.ts
+++ packages/typescript/src/configs/getFormatCodeSettings.ts
@@ -14,32 +14,32 @@ export async function getFormatCodeSettings(
 
 	config = config ?? {};
 
-	const defaultFormatOptions = ctx.typescript.module.getDefaultFormatCodeSettings()
+	const defaultFormatOptions = ctx.typescript.module.getDefaultFormatCodeSettings();
 
 	return Object.assign({}, defaultFormatOptions, filterUndefined({
-        convertTabsToSpaces: options?.insertSpaces,
-        tabSize: options?.tabSize,
-        indentSize: options?.tabSize,
-        indentStyle: 2 /** ts.IndentStyle.Smart */,
-        newLineCharacter: '\n',
-        insertSpaceAfterCommaDelimiter: config.insertSpaceAfterCommaDelimiter ?? true,
-        insertSpaceAfterConstructor: config.insertSpaceAfterConstructor ?? false,
-        insertSpaceAfterSemicolonInForStatements: config.insertSpaceAfterSemicolonInForStatements ?? true,
-        insertSpaceBeforeAndAfterBinaryOperators: config.insertSpaceBeforeAndAfterBinaryOperators ?? true,
-        insertSpaceAfterKeywordsInControlFlowStatements: config.insertSpaceAfterKeywordsInControlFlowStatements ?? true,
-        insertSpaceAfterFunctionKeywordForAnonymousFunctions: config.insertSpaceAfterFunctionKeywordForAnonymousFunctions ?? true,
-        insertSpaceBeforeFunctionParenthesis: config.insertSpaceBeforeFunctionParenthesis ?? false,
-        insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: config.insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis ?? false,
-        insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets: config.insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets ?? false,
-        insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces: config.insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces ?? true,
-        insertSpaceAfterOpeningAndBeforeClosingEmptyBraces: config.insertSpaceAfterOpeningAndBeforeClosingEmptyBraces ?? true,
-        insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces: config.insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces ?? false,
-        insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces: config.insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces ?? false,
-        insertSpaceAfterTypeAssertion: config.insertSpaceAfterTypeAssertion ?? false,
-        placeOpenBraceOnNewLineForFunctions: config.placeOpenBraceOnNewLineForFunctions ?? false,
-        placeOpenBraceOnNewLineForControlBlocks: config.placeOpenBraceOnNewLineForControlBlocks ?? false,
-        semicolons: config.semicolons ?? 'ignore',
-    }));
+		convertTabsToSpaces: options?.insertSpaces,
+		tabSize: options?.tabSize,
+		indentSize: options?.tabSize,
+		indentStyle: 2 /** ts.IndentStyle.Smart */,
+		newLineCharacter: '\n',
+		insertSpaceAfterCommaDelimiter: config.insertSpaceAfterCommaDelimiter ?? true,
+		insertSpaceAfterConstructor: config.insertSpaceAfterConstructor ?? false,
+		insertSpaceAfterSemicolonInForStatements: config.insertSpaceAfterSemicolonInForStatements ?? true,
+		insertSpaceBeforeAndAfterBinaryOperators: config.insertSpaceBeforeAndAfterBinaryOperators ?? true,
+		insertSpaceAfterKeywordsInControlFlowStatements: config.insertSpaceAfterKeywordsInControlFlowStatements ?? true,
+		insertSpaceAfterFunctionKeywordForAnonymousFunctions: config.insertSpaceAfterFunctionKeywordForAnonymousFunctions ?? true,
+		insertSpaceBeforeFunctionParenthesis: config.insertSpaceBeforeFunctionParenthesis ?? false,
+		insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: config.insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis ?? false,
+		insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets: config.insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets ?? false,
+		insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces: config.insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces ?? true,
+		insertSpaceAfterOpeningAndBeforeClosingEmptyBraces: config.insertSpaceAfterOpeningAndBeforeClosingEmptyBraces ?? true,
+		insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces: config.insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces ?? false,
+		insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces: config.insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces ?? false,
+		insertSpaceAfterTypeAssertion: config.insertSpaceAfterTypeAssertion ?? false,
+		placeOpenBraceOnNewLineForFunctions: config.placeOpenBraceOnNewLineForFunctions ?? false,
+		placeOpenBraceOnNewLineForControlBlocks: config.placeOpenBraceOnNewLineForControlBlocks ?? false,
+		semicolons: config.semicolons ?? 'ignore',
+	}));
 }
 
 function filterUndefined<T extends Record<string, any>>(obj: T) {
