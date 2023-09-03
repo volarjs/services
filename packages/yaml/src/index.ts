55f62b807a946cecbb58998e5cd5ab33e28d110f
Johnson Chu <johnsoncodehk@gmail.com>
2023-09-03 13:56:46 +0800

    chore: unused umd path for now

--- packages/yaml/src/index.ts
+++ packages/yaml/src/index.ts
@@ -1,7 +1,7 @@
 import { type Service } from '@volar/language-service';
 import { type TextDocument } from 'vscode-languageserver-textdocument';
 import { type LanguageSettings, type LanguageService } from 'yaml-language-server';
-import { getLanguageService } from 'yaml-language-server/lib/umd/languageservice/yamlLanguageService.js';
+import { getLanguageService } from 'yaml-language-server';
 
 export interface Provide {
 	'yaml/languageService': () => LanguageService;
