3c7137df94587a2b22ac1d354fbe338a10ba4bc6
Johnson Chu <johnsoncodehk@gmail.com>
2023-10-25 15:46:43 +0800

    fix(emmet, pug): browser stub missing create() function

--- packages/pug/src/empty.ts
+++ packages/pug/src/empty.ts
@@ -2,4 +2,8 @@ import type { Service } from '@volar/language-service';
 
 console.warn('volar-service-pug: this module is not yet supported for web.');
 
-export default (): Service => () => ({});
+export function create(): Service {
+	return () => ({});
+}
+
+export default create;
