b2b3611cf478d46b769fa582e4f5d89c10e4a886
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-02-28 18:31:36 +0100

    feat(prettier): Allow a Prettier instance to be optional

--- packages/prettier/index.ts
+++ packages/prettier/index.ts
@@ -44,6 +44,10 @@ export function create(
 		 */
 		prettier?: typeof import('prettier') | undefined;
 		getPrettier?: (serviceEnv: ServiceEnvironment) => typeof import('prettier') | undefined,
+		/**
+		 * If true, the plugin will not throw an error if it can't load Prettier either through the `prettier`, or `getPrettier` properties or through a normal `import('prettier')`.
+		 */
+		allowImportError?: boolean;
 	} = {},
 	getPrettierConfig = async (filePath: string, prettier: typeof import('prettier'), config?: ResolveConfigOptions) => {
 		return await prettier.resolveConfig(filePath, config) ?? {};
@@ -59,12 +63,13 @@ export function create(
 					?? options.getPrettier?.(context.env)
 					?? require('prettier');
 			} catch (e) {
-				throw new Error("Could not load Prettier: " + e);
+				if (!options.allowImportError) throw new Error("Could not load Prettier: " + e);
 			}
 			const languages = options.languages ?? ['html', 'css', 'scss', 'typescript', 'javascript'];
 
 			return {
 				async provideDocumentFormattingEdits(document, _, formatOptions) {
+					if (!prettier) return;
 					if (!languages.includes(document.languageId)) {
 						return;
 					}
