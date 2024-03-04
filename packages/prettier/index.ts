e4c5a531b2a49e1cf12aa66f6a503223dd60f6b2
Johnson Chu <johnsoncodehk@gmail.com>
2024-03-05 03:08:50 +0800

    Apply @remcohaszing suggest

--- packages/prettier/index.ts
+++ packages/prettier/index.ts
@@ -58,83 +58,91 @@ export function create(
 		create(context): ServicePluginInstance {
 			const languages = options.languages ?? ['html', 'css', 'scss', 'typescript', 'javascript'];
 
-			let prettier: typeof import('prettier');
+			let prettier = options.prettier;
 			try {
-				prettier = options.prettier
-					?? options.getPrettier?.(context.env)
-					?? require('prettier');
-
-				return {
-					async provideDocumentFormattingEdits(document, _, formatOptions) {
-						if (!prettier) return;
-						if (!languages.includes(document.languageId)) {
-							return;
-						}
-
-						const filePath = URI.parse(document.uri).fsPath;
-						const fileInfo = await prettier.getFileInfo(filePath, { ignorePath: '.prettierignore', resolveConfig: false });
-
-						if (fileInfo.ignored) {
-							return;
-						}
-
-						const filePrettierOptions = await getPrettierConfig(
-							filePath,
-							prettier,
-							options.resolveConfigOptions
-						);
-
-						const editorPrettierOptions = await context.env.getConfiguration?.('prettier', document.uri);
-						const ideFormattingOptions =
-							formatOptions !== undefined && options.useIdeOptionsFallback // We need to check for options existing here because some editors might not have it
-								? {
-									tabWidth: formatOptions.tabSize,
-									useTabs: !formatOptions.insertSpaces,
-								}
-								: {};
-
-						// Return a config with the following cascade:
-						// - Prettier config file should always win if it exists, if it doesn't:
-						// - Prettier config from the VS Code extension is used, if it doesn't exist:
-						// - Use the editor's basic configuration settings
-						const prettierOptions = returnObjectIfHasKeys(filePrettierOptions) || returnObjectIfHasKeys(editorPrettierOptions) || ideFormattingOptions;
-
-						const currentPrettierConfig: Options = {
-							...(options.additionalOptions
-								? await options.additionalOptions(prettierOptions)
-								: prettierOptions),
-							filepath: filePath,
-						};
-
-						if (!options.ignoreIdeOptions) {
-							currentPrettierConfig.useTabs = !formatOptions.insertSpaces;
-							currentPrettierConfig.tabWidth = formatOptions.tabSize;
-						}
-
-						const fullText = document.getText();
-						let oldText = fullText;
-
-						const isHTML = document.languageId === "html";
-						if (isHTML && options.html?.breakContentsFromTags) {
-							oldText = oldText
-								.replace(/(<[a-z][^>]*>)([^ \n])/gi, "$1 $2")
-								.replace(/([^ \n])(<\/[a-z][a-z0-9\t\n\r -]*>)/gi, "$1 $2");
-						}
-
-						return [{
-							newText: await prettier.format(oldText, currentPrettierConfig),
-							range: {
-								start: document.positionAt(0),
-								end: document.positionAt(fullText.length),
-							},
-						}];
-					},
+				if (!prettier) {
+					if (options.getPrettier) {
+						prettier = options.getPrettier(context.env);
+					} else {
+						prettier = require('prettier');
+					}
+				}
+			} catch (error) {
+				if (!options.allowImportError) {
+					throw new Error("Could not load Prettier: ");
 				};
-			} catch (e) {
-				if (!options.allowImportError) throw new Error("Could not load Prettier: " + e);
 			}
 
-			return {};
+			if (!prettier) {
+				return {};
+			}
+
+			return {
+				async provideDocumentFormattingEdits(document, _, formatOptions) {
+					if (!prettier) return;
+					if (!languages.includes(document.languageId)) {
+						return;
+					}
+
+					const filePath = URI.parse(document.uri).fsPath;
+					const fileInfo = await prettier.getFileInfo(filePath, { ignorePath: '.prettierignore', resolveConfig: false });
+
+					if (fileInfo.ignored) {
+						return;
+					}
+
+					const filePrettierOptions = await getPrettierConfig(
+						filePath,
+						prettier,
+						options.resolveConfigOptions
+					);
+
+					const editorPrettierOptions = await context.env.getConfiguration?.('prettier', document.uri);
+					const ideFormattingOptions =
+						formatOptions !== undefined && options.useIdeOptionsFallback // We need to check for options existing here because some editors might not have it
+							? {
+								tabWidth: formatOptions.tabSize,
+								useTabs: !formatOptions.insertSpaces,
+							}
+							: {};
+
+					// Return a config with the following cascade:
+					// - Prettier config file should always win if it exists, if it doesn't:
+					// - Prettier config from the VS Code extension is used, if it doesn't exist:
+					// - Use the editor's basic configuration settings
+					const prettierOptions = returnObjectIfHasKeys(filePrettierOptions) || returnObjectIfHasKeys(editorPrettierOptions) || ideFormattingOptions;
+
+					const currentPrettierConfig: Options = {
+						...(options.additionalOptions
+							? await options.additionalOptions(prettierOptions)
+							: prettierOptions),
+						filepath: filePath,
+					};
+
+					if (!options.ignoreIdeOptions) {
+						currentPrettierConfig.useTabs = !formatOptions.insertSpaces;
+						currentPrettierConfig.tabWidth = formatOptions.tabSize;
+					}
+
+					const fullText = document.getText();
+					let oldText = fullText;
+
+					const isHTML = document.languageId === "html";
+					if (isHTML && options.html?.breakContentsFromTags) {
+						oldText = oldText
+							.replace(/(<[a-z][^>]*>)([^ \n])/gi, "$1 $2")
+							.replace(/([^ \n])(<\/[a-z][a-z0-9\t\n\r -]*>)/gi, "$1 $2");
+					}
+
+					return [{
+						newText: await prettier.format(oldText, currentPrettierConfig),
+						range: {
+							start: document.positionAt(0),
+							end: document.positionAt(fullText.length),
+						},
+					}];
+				},
+			};
 		},
 	};
 }
