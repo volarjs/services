6382e0f61e1501d9fe94e9787089a551dfa53305
Johnson Chu <johnsoncodehk@gmail.com>
2024-05-10 16:20:54 +0800

    chore: warning message format

--- packages/emmet/empty.ts
+++ packages/emmet/empty.ts
@@ -1,6 +1,6 @@
 import type { LanguageServicePlugin } from '@volar/language-service';
 
-console.warn('volar-service-emmet: this module is not yet supported for web.');
+console.warn('[volar-service-emmet] this module is not yet supported for web.');
 
 export function create(): LanguageServicePlugin {
 	return {
