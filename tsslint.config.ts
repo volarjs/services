f6c96bad44880bec3ae60761d36b3d4d6b4f53cd
Johnson Chu <johnsoncodehk@gmail.com>
2024-06-09 11:37:24 +0800

    lint: use `getDefaultRules()` from Volar TSSLint config

--- tsslint.config.ts
+++ tsslint.config.ts
@@ -1,1 +1,8 @@
-export default (await import('https://raw.githubusercontent.com/volarjs/volar.js/master/tsslint.config.ts')).default;
+import { defineConfig } from '@tsslint/config';
+import { getDefaultRules as getDefaultVolarRules } from 'https://raw.githubusercontent.com/volarjs/volar.js/master/tsslint.config.ts';
+
+export default defineConfig({
+	rules: {
+		...getDefaultVolarRules(),
+	},
+});
